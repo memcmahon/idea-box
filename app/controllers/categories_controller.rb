15ee840e66e72e75a2fb61122026ba3476ef5b21
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-10 12:29:27 -0700

    Adds delete controller to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -26,6 +26,12 @@ class CategoriesController < ApplicationController
     redirect_to categories_path
   end
 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
   private
 
   def category_params
