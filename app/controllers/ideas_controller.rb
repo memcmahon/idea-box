1655e8787c1fbe084bc810f4fa9ca5947df79061
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-11 13:55:47 -0700

    Updates idea params to allow category id

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -9,9 +9,26 @@ class IdeasController < ApplicationController
   end
 
   def new
+    @categories = Category.all
     @user = User.find(params[:user_id])
     @idea = @user.ideas.new()
-    @categories = Category.all
-    @images = Image.all
+    binding.pry
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @idea = @user.ideas.new(idea_params)
+    if @idea.save
+      flash[:notice] = "Success"
+      redirect_to user_idea_path(@user, @idea)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :contents, :category_id, :user)
   end
 end
