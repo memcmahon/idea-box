195ae09ca4839241f07d43a3c97208b10dc59933
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-12 14:07:09 -0700

    Adds test for user adding multiple images to an idea

--- spec/features/idea_images/user_can_add_images_to_ideas_spec.rb
+++ spec/features/idea_images/user_can_add_images_to_ideas_spec.rb
@@ -20,5 +20,20 @@ describe "user can add a images to an idea" do
       expect(page).to have_xpath("//img[contains(@src,'https://images.unsplash.com/photo-1495554698253-681539e9ea84?ixlib=rb-0.3.5&s=72f6aabe1c8a038b8f4f6adfac6a5fba&auto=format&fit=crop&w=2850&q=80')]")
       expect(page).to have_content("Heliskiing")
     end
+
+    it "they can add multiple images to that idea" do
+      visit user_idea_path(@user, @idea)
+
+      select("Heliskiing", :from => "idea_image[image_id]")
+      click_on("Add")
+
+      select("Virgin Gorda", :from => "idea_image[image_id]")
+      click_on("Add")
+
+      expect(page).to have_xpath("//img[contains(@src,'https://images.unsplash.com/photo-1495554698253-681539e9ea84?ixlib=rb-0.3.5&s=72f6aabe1c8a038b8f4f6adfac6a5fba&auto=format&fit=crop&w=2850&q=80')]")
+      expect(page).to have_content("Heliskiing")
+      expect(page).to have_xpath("//img[contains(@src,'https://s-media-cache-ak0.pinimg.com/originals/d3/e8/cb/d3e8cba6aa1c3cb52c8e0169a18e4e08.jpg')]")
+      expect(page).to have_content("Virgin Gorda")
+    end
   end
 end
