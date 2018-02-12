b94d01e0e2dfd94ea1d8409ed64c24b33084049b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-11 18:42:46 -0700

    Adds create action for images controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,4 +3,18 @@ class ImagesController < ApplicationController
     @images = Image.all
     @image = Image.new()
   end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:notice] = "Success"
+      redirect_to images_path
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url, :alt_tag)
+  end
 end
