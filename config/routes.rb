2a5f277d035f0aee8d5eb67e501547c2a70a3350
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-12 15:25:56 -0700

    Adds destroy route to idea_images

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :images, except:[:new, :show]
 
-  resources :idea_images, only:[:create]
+  resources :idea_images, only:[:create, :destroy]
 
   resources :users, only:[:show] do
     resources :ideas
