7624b6f1a9b954fa741644c7b49d6ae0020fc05e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-12 20:49:04 -0700

    Updates log in test to use email instead of username

--- spec/features/users/user_can_log_in_spec.rb
+++ spec/features/users/user_can_log_in_spec.rb
@@ -10,7 +10,7 @@ describe "when a user logs in" do
 
     expect(current_path).to eq(login_path)
 
-    fill_in "username", with: user.username
+    fill_in "email", with: user.email
     fill_in "password", with: user.password
 
     click_on "Log In"
