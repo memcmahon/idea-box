a65442cf3165a2adc5efa7913d9d8d340501675b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-13 11:06:08 -0700

    Adds model tests for user roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,27 @@ describe User, type: :model do
   describe "Relationships" do
     it { should have_many :ideas }
   end
+
+  describe "Roles" do
+    it "can be created as an admin" do
+      admin = User.create!(first_name: "Jane", last_name: "Doe", email: "jane@fakemail.com", password: "test", role: 1)
+
+      expect(admin.role).to eq("admin")
+      expect(admin.admin?).to be_truthy
+    end
+
+    it "can be created as a regular user" do
+      user = User.create!(first_name: "Jane", last_name: "Doe", email: "jane@fakemail.com", password: "test", role: 0)
+
+      expect(admin.role).to eq("default")
+      expect(admin.default?).to be_truthy
+    end
+
+    it "can be defaulted to a regular user" do
+      user = User.create!(first_name: "Jane", last_name: "Doe", email: "jane@fakemail.com", password: "test")
+
+      expect(admin.role).to eq("default")
+      expect(admin.default?).to be_truthy
+    end
+  end
 end
