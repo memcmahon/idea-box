0add28020a71caf7f4b26f6251c2fa651f495491
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-13 11:41:04 -0700

    Updates category index feature test for admin authorization

--- spec/features/categories/user_can_see_all_categories_spec.rb
+++ spec/features/categories/user_can_see_all_categories_spec.rb
@@ -1,18 +1,36 @@
 require 'rails_helper'
 
-describe "user can see a list of all categories" do
-  describe "they visit categories_path" do
+describe "users can see a list of all categories" do
+  describe "as an admin they visit categories_path" do
     it "they see all categories" do
-      adventure = Category.create!(name: "Adventure")
-      relax = Category.create!(name: "Relax")
-      volunteer = Category.create!(name: "Volunteer")
+      admin = User.create(first_name: "Jane", last_name: "Doe", email: "jane@fakemail.com", password: "test", role: 1)
+      category_1 = Category.create!(name: "Adventure")
+      category_2 = Category.create!(name: "Relax")
+      category_3 = Category.create!(name: "Volunteer")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit categories_path
 
-      expect(page).to have_content ("Categories")
-      expect(page).to have_content ("Adventure")
-      expect(page).to have_content ("Relax")
-      expect(page).to have_content ("Volunteer")
+      expect(page).to have_content("Categories")
+      expect(page).to have_content(category_1.name)
+      expect(page).to have_content(category_2.name)
+      expect(page).to have_content(category_3.name)
+    end
+  end
+
+  describe "as regular user they visit categories_path" do
+    it "they see an error message categories" do
+      user = User.create(first_name: "Jane", last_name: "Doe", email: "jane@fakemail.com", password: "test")
+      category_1 = Category.create!(name: "Adventure")
+      category_2 = Category.create!(name: "Relax")
+      category_3 = Category.create!(name: "Volunteer")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit categories_path
+
+      expect(page).to have_content("The page you were looking for does not exist")
     end
   end
 end
