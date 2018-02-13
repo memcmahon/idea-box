b1d6afd9c2e46d6af52e82dc0b7166d712d37647
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-12 17:55:30 -0700

    Adds model test for idea

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Idea, type: :model do
+  describe "Validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :content}
+  end
+
+  describe "Relationships" do
+    it {should belong_to(:user)}
+    it {should belong_to(:category)}
+    it {should have_many(:images).through(:idea_images)}
+  end
+end
