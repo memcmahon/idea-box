fda393382bd08b75cb8a867294d41622b256bcf0
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-12 20:36:46 -0700

    Adds feature test for user login

---
+++ spec/features/users/user_can_log_in_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "when a user logs in" do
+  it "they see their personal info" do
+    user = User.create!(first_name: "Jane", last_name: "Doe", email: "jane@fakemail.com", password: "test")
+
+    visit root_path
+
+    click_on "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "username", with: user.username
+    fill_in "password", with: user.password
+
+    click_on "Log In"
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content("Welcome, #{user.username}")
+  end
+end
