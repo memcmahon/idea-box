a807c8078a257407ec6271b337b670e2f18ae33c
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-12 18:53:22 -0700

    Updates db structure with encrypted password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211003958) do
+ActiveRecord::Schema.define(version: 20180213014717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,9 +52,9 @@ ActiveRecord::Schema.define(version: 20180211003958) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "idea_images", "ideas"
