4ae257a2db15e3cf7ef15b818cd2e019dc66d8aa
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-12 17:57:56 -0700

    Updates category_spec for shouldas

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 
 describe Category, type: :model do
   describe "Validations" do
-    it "is invalid without a name" do
-      category = Category.new
-
-      expect(category).to_not be_valid
-    end
+    it { should validate_presence_of :name }
   end
 
   describe "Relationships" do
-    it {should have_many(:ideas)}
+    it { should have_many(:ideas) }
   end
 end
