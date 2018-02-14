bbbd76a23628cdb60e0c0f4b0604617f6bdd2280
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-14 14:04:23 -0700

    Adds idea-images dependency for destroy action

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,6 +3,6 @@ class Idea < ApplicationRecord
 
   belongs_to :user
   belongs_to :category
-  has_many :idea_images
+  has_many :idea_images, dependent: :destroy
   has_many :images, through: :idea_images
 end
