3d46533abbb6b7acab91309ad176e23b0805882c
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-15 13:44:39 -0700

    Refactors nav bar for better control by current user

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,8 +2,6 @@ class IdeasController < ApplicationController
   before_action :verify_user
   before_action :set_idea, only: [:show, :edit, :update, :destroy]
 
-  layout "user_layout"
-
   def index
     @ideas = current_user.ideas
   end
